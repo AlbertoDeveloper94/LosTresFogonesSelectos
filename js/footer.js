9613a97c10a51b34a2076e243aa0dc7f882049a1
Mariaregue-spec <mariareguecurso@gmail.com>
2025-12-05 13:44:42 +0100

    feat:js

--- js/footer.js
+++ js/footer.js
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-        <link rel="stylesheet" href="/css/footer.css">
-        <title>Footer - Los Tres Fogones Selectos</title>
-</head>
-<body>
-<my-footer>
-<footer class="footer">
+class MyFooter extends HTMLElement {
+connectedCallback() {
+    this.innerHTML = `
+
+
+    <footer>
         <div class="container text-center">
             <div class="row align-items-center">
                 <div class="col">
@@ -36,10 +29,7 @@
                 </div>
             </div>
     </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-        crossorigin="anonymous"></script>
-    <script src="js/script.js"></script>
-</body>
-</html>
+    `;
+    }
+}
+customElements.define("footer-los-tres-fogones-selectos", MyFooter);
