d4124e2d084374fa91d168609e4c119de8cb8207
webermuros1 <webermuros1@gmail.com>
2025-12-10 22:21:01 +0100

    reset

--- js/header.js
+++ js/header.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 class header extends HTMLElement {
 connectedCallback() {
@@ -98,3 +99,40 @@ connectedCallback() {
     }
 }
 customElements.define("header-los-tres-fogones-selectos", header);
+=======
+class MyHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            </header>
+    `;
+
+    }
+customElements.define("footer-los-tres-fogones-selectos", MyHeader);
+}
+>>>>>>> be7652f (reset)
