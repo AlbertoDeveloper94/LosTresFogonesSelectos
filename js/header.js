e094f4a385c373d15bcfdc1569c188a15be1b7a9
webermuros1 <webermuros1@gmail.com>
2025-12-11 18:09:18 +0100

    delate exta merge information

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
 class MyHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-    
+
 
 
 
@@ -15,7 +15,15 @@ class MyHeader extends HTMLElement {
                 src="../Images/logoLosTresFogonesSelectos.png"
                 alt="logo"
                 class="classlogo"
-  
+
+            />
+            Los tres fogones selectos
+            </a>
+
+            <!-- Botón menú móvil -->
+            <button
+
+
             class="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
